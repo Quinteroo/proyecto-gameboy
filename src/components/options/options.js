51763c2fb2e20b10e89fa08162623ee5cda2b790
quintero git <hello.quinteroo@gmail.com>
2024-11-21 18:36:31 +0100

    CREATE options component

--- src/components/options/options.js
+++ src/components/options/options.js
@@ -6,21 +6,24 @@ export const options = () => {
   options.classList.add("options")
 
   const liGithub = document.createElement("li")
-  const buttonGithub = document.createElement("button")
+  const aGithub = document.createElement("a")
+  aGithub.href = "https://github.com/Quinteroo"
+  aGithub.target = "_blank"
+
   const pGithub = document.createElement("p")
   pGithub.textContent = "GITHUB"
 
-  liGithub.appendChild(buttonGithub)
+  liGithub.appendChild(aGithub)
   liGithub.appendChild(pGithub)
 
   options.appendChild(liGithub)
 
   const liStart = document.createElement("li")
-  const buttonStart = document.createElement("button")
+  const aStart = document.createElement("a")
   const pStart = document.createElement("p")
   pStart.textContent = "START"
 
-  liStart.appendChild(buttonStart)
+  liStart.appendChild(aStart)
   liStart.appendChild(pStart)
 
   options.appendChild(liStart)
